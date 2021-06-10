684c38806cd20e71aafdd1d98d338701ac4a5072
Austin Cox <austincox@Lordinateur.local>
2021-06-10 05:19:53 -0400

    completed initial data functions

--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'localhost:5432/linkerator'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
+const { Client } = require("pg");
+const DB_NAME = "localhost:5432/linkerator";
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
+const bcrypt = require("bcrypt");
 
 // database methods
 
-//methods to create 
+//methods to create
 //getAllLinks
 //getTagsById
 //getAllTags
@@ -16,8 +17,89 @@ const client = new Client(DB_URL);
 //destroyTag
 //getSortedLinks
 
+//
+
+const createUser = async ({ username, password }) => {
+  const hashedPassword = await hashPassword(password);
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    INSERT INTO users(username, password) VALUES ($1, $2)
+      RETURNING id, username;
+    `,
+      [username, hashedPassword]
+    );
+    console.log("user row", user);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const createLink = async ({ creatorId, title, url, clickCount, date }) => {
+  try {
+    const {
+      rows: [link],
+    } = await client.query(
+      `
+    INSERT INTO links("creatorId", title, url, "clickCount", date) VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `,
+      [creatorId, title, url, clickCount, date]
+    );
+    return link;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createComment = async ({ linkId, body }) => {
+  try {
+    const {
+      rows: [comment],
+    } = await client.query(
+      `
+    INSERT INTO comments("linkId", body) VALUES ($1, $2) RETURNING *;
+    `,
+      [linkId, body]
+    );
+    return comment;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createTag = async ({ linkId, tagName }) => {
+  try {
+    const {
+      rows: [tag],
+    } = await client.query(
+      `
+    INSERT INTO tags("linkId", "tagName") VALUES ($1, $2) RETURNING *;
+    `,
+      [linkId, tagName]
+    );
+    return tag;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//util functions
+async function hashPassword(password) {
+  const SALT_COUNT = 10;
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+  return hashedPassword;
+}
+
 // export
 module.exports = {
   client,
   // db methods
-}
+  createUser,
+  createLink,
+  createComment,
+  createTag,
+};
--- db/init_db.js
+++ db/init_db.js
@@ -2,28 +2,221 @@
 const {
   client,
   // other db methods
+  createUser,
+  createLink,
+  createComment,
+  createTag,
 } = require("./index");
 
-async function buildTables() {
+const buildTables = async () => {
   try {
     client.connect();
     // drop tables in correct order
     console.log("Dropping all tables...");
     await client.query(`
-    DROP TABLE IF EXISTS links;
+    DROP TABLE IF EXISTS comments;
     DROP TABLE IF EXISTS tags;
+    DROP TABLE IF EXISTS links;
+    DROP TABLE IF EXISTS users;
    `);
     console.log("Tables dropped succesfully");
 
     // build tables in correct order
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+    );
+      CREATE TABLE links(
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        title varchar(255) UNIQUE NOT NULL,
+        url varchar(255) UNIQUE NOT NULL,
+        "clickCount" INTEGER DEFAULT 0,
+        date DATE NOT NULL
+    );
+      CREATE TABLE comments(
+        id SERIAL PRIMARY KEY,
+        "linkId" INTEGER REFERENCES links(id),
+        body varchar(255) NOT NULL
+    );
+      CREATE TABLE tags(
+        id SERIAL PRIMARY KEY,
+        "linkId" INTEGER REFERENCES links(id),
+        "tagName" varchar(255) NOT NULL
+    );
+  `);
+    console.log("Tables created succesfully");
   } catch (error) {
+    console.error("error dropping/creating tables");
     throw error;
   }
-}
+};
+
+const createInitialUsers = async () => {
+  try {
+    console.log("Starting to create users...");
+    const usersToCreate = [
+      { username: "albert", password: "bertie99" },
+      { username: "sandra", password: "sandra123" },
+      { username: "glamgal", password: "glamgal123" },
+    ];
+    const users = await Promise.all(usersToCreate.map(createUser));
+    console.log("Users created:");
+    console.log(users);
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createInitialLinks = async () => {
+  try {
+    console.log("Starting to create links...");
+    const linksToCreate = [
+      {
+        creatorId: 1,
+        title: "Google",
+        url: "https://www.google.com",
+        clickCount: 0,
+        date: "2021-06-10",
+      },
+      {
+        creatorId: 1,
+        title: "YouTube",
+        url: "https://www.youtube.com",
+        clickCount: 0,
+        date: "2021-05-14",
+      },
+      {
+        creatorId: 2,
+        title: "W3Schools",
+        url: "https://www.w3schools.com/",
+        clickCount: 0,
+        date: "2021-01-17",
+      },
+      {
+        creatorId: 2,
+        title: "Netflix",
+        url: "https://www.netflix.com/",
+        clickCount: 0,
+        date: "2021-03-28",
+      },
+      {
+        creatorId: 3,
+        title: "Twitter",
+        url: "https://www.twitter.com/",
+        clickCount: 0,
+        date: "2020-12-13",
+      },
+    ];
+    const links = await Promise.all(linksToCreate.map(createLink));
+    console.log("Links created:");
+    console.log(links);
+    console.log("Finished creating links!");
+  } catch (error) {
+    console.log("Error creating links!");
+    console.error(error);
+  }
+};
+
+const createInitialComments = async () => {
+  try {
+    console.log("Starting to create comments...");
+    const commentsToCreate = [
+      {
+        linkId: 1,
+        body: "Probably the best website ever!",
+      },
+      {
+        linkId: 1,
+        body: "The best!!",
+      },
+      {
+        linkId: 2,
+        body: "Check this out!",
+      },
+      {
+        linkId: 3,
+        body: "Wow... just wow.",
+      },
+      {
+        linkId: 4,
+        body: "My favorite :)",
+      },
+      {
+        linkId: 4,
+        body: "This is the coolest site omg",
+      },
+    ];
+    const comments = await Promise.all(commentsToCreate.map(createComment));
+    console.log("Comments created:");
+    console.log(comments);
+    console.log("Finished creating comments!");
+  } catch (error) {
+    console.log("Error creating comments!");
+    console.error(error);
+  }
+};
+
+const createInitialTags = async () => {
+  try {
+    console.log("Starting to create tags...");
+
+    const tagsToCreate = [
+      {
+        linkId: 1,
+        tagName: "Favorite",
+      },
+      {
+        linkId: 1,
+        tagName: "School",
+      },
+      {
+        linkId: 2,
+        tagName: "Favorite",
+      },
+      {
+        linkId: 2,
+        tagName: "Video",
+      },
+      {
+        linkId: 2,
+        tagName: "Entertainment",
+      },
+      {
+        linkId: 3,
+        tagName: "School",
+      },
+      {
+        linkId: 4,
+        tagName: "Entertainment",
+      },
+      {
+        linkId: 4,
+        tagName: "Favorite",
+      },
+    ];
+    const tags = await Promise.all(tagsToCreate.map(createTag));
+    console.log("Tags created:");
+    console.log(tags);
+    console.log("Finished creating tags!");
+    return tags;
+  } catch (error) {
+    console.log("Error creating tags!");
+    console.error(error);
+  }
+};
 
 async function populateInitialData() {
   try {
-    // create useful starting data
+    await createInitialUsers();
+    await createInitialLinks();
+    await createInitialComments();
+    await createInitialTags();
   } catch (error) {
     throw error;
   }
@@ -33,3 +226,5 @@ buildTables()
   .then(populateInitialData)
   .catch(console.error)
   .finally(() => client.end());
+
+//changed click-count to camelcase clickCount bc it is used in multiple functions.
