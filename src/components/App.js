1e22e741c35a64558d24bb44c0c81ac51e02ed1c
Kasey <klcampbell36@gmail.com>
2021-06-20 11:01:39 -0400

    search bar/side nav

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,72 @@ import { getSomething } from "../api";
 import '../css/style.css'
 
 const App = () => {
-  const [message, setMessage] = useState("");
   const [links, setLinks] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchOption, setSearchOption] = useState('')
+  const [sortOption, setSortOption] = useState('');
 
   // useEffect( async () => {
   //   const response = await fetch();
   //   const links = response.json();
   //   console.log({links});
   // })
+
+  useEffect(async () => {
+    fetchAPI('/routes/links')
+      .then((resp) => {
+        console.log("Here are the links:", resp)
+        let sortedList = resp.sort((a, b) => (a.clickcount > b.clickcount)? -1: 1);
+        setLinks(sortedList);
+      })
+      .catch(console.error);
+  }, []);
+  
+  useEffect(async () => {
+  fetchAPI('/routes/tags')
+    .then((data) => {
+      console.log(data)
+      setTagList(data)
+    })
+    .catch(console.error)
+}, [])
+
+
+
+  function filteredLinks() {
+    if (searchOption === 'Tags') {
+      return links.filter((_link) => {
+        let tags = _link.tags.map((tag) => {
+          return tag.tag;
+        })
+        if (tags.join(', ').includes(search)) {
+          return _link;
+        }
+      })
+    }
+    return links.filter((_link) => {
+      return _link.link.includes(search.toLowerCase());
+    });
+  }
+
+  return (
+  <Route path='/'>
+  <SearchBar 
+    search={search}
+    setSearch={setSearch}
+    setSearchOption={setSearchOption}
+    searchOption={searchOption}
+    sortOption={sortOption}
+    setSortOption={setSortOption}
+    links={links}
+    setLinks={setLinks}/>
+  <LinkTable
+    links={filteredLinks()}
+    setSearch={setSearch}
+    setLinks={setLinks}/>
+</Route>
+)
+};
 
   useEffect(() => {
     getSomething()
